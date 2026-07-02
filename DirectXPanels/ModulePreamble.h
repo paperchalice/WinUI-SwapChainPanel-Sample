94b10a178dec91d4da6d11ba82083024c5987611
PaperChalice <liujunchang97@outlook.com>
2026-07-02 21:56:07 +0800

    Add workaround for C++WinRT module in WinUI project

---
+++ DirectXPanels/ModulePreamble.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Workaround from https://github.com/microsoft/cppwinrt/blob/master/nuget/modules.md
+
+#define WINRT_IMPORT_MODULE
+
+// Import the C++/WinRT namespaces used across this project
+import winrt.Windows.Foundation;
+import winrt.Windows.Foundation.Collections;
+import winrt.Windows.System;
+// ... other namespaces your project needs
+
+// Component modules
+import winrt.DirectXPanels;
